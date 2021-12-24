4e1f9bce8cc4f8a85f7b18d13b1db6b08026cd25
adezxc <adam.jasinski@unity3d.com>
2021-12-25 00:55:52 +0200

    complete lab3

--- l3/main.s
+++ l3/main.s
@@ -3,12 +3,11 @@
 fin:	.asciz "input.csv"
 fout:   .asciz "output.txt"
 start: .asciz "Adam Jasinski, Bioinformatika, 2 kursas\n"
-nl:     .asciz "\n"
-result: .asciz "Trumpiausias žodis pirmame ir antrame stulpeliuose yra: "
+result: .asciz "Trumpiausias žodis pirmame ir antrame stulpeliuose yra:"
 skaitymas: .word 0x0
+paskutinisSkaitymas: .word 0x0
 rasymas: .word 0x0
 buf: .space 0x1000
-shortestWord: .space 0x1000
 
 .macro readSymbol
   li  a7, 63
@@ -17,12 +16,14 @@ shortestWord: .space 0x1000
   li  a2, 1
   ecall
   ble a0, x0, end
+  addi s5, s5, 1
   lbu a0, buf
 .end_macro
 
         .text
   # Atidaryti failą skaitymui
 main:
+  li   s3, 100
   li   a7, 1024
   la   a0, fin
   li   a1, 0
@@ -45,15 +46,14 @@ main:
   j skipLine
   li s4, 0        # Dabartinio žodžio ilgis
   loop:
-  	li t2, 2
   	readSymbol
   	jal checkSemicolon
   	jal checkSpace
+  	continue:
   	addi s4, s4, 1
-  	beq s2, t2, skipLine
-  	
+  	newResult:
   	j loop
-
+	
 
 	
 skipLine:
@@ -62,47 +62,89 @@ skipLine:
   jal checkNewLine
   j skipLine
 	
-checkSpace:
-  li t1, 0x20
-  beq a0, t1, recordWord
-  jr x1
-  
-recordWord:
-  li a7, 62
-  la a0, skaitymas
-  li a1, 0
-  li a2, 1
-  ecall
-  li a7, 63
-  la a1, shortestWord
-  mv a2, s4
-  ecall
-  li a7, 64
-  la a0, rasymas
-  la a1, shortestWord
-  mv a2, s4
-  ecall
-  jr x1
-	
 checkNewLine:
   li t1, 0x0A
   beq a0, t1, loop
   jr x1
-  
+	
 checkSemicolon:
   li t1, 0x3B
   beq a0, t1, incSemicolon
+  continueSemicolon:
+  beq a0, t1, checkWord
   jr x1
   
 incSemicolon:
     addi s2, s2, 1
-    jr x1
+    li t2, 2
+    beq s2, t2, skipLine
+    j continueSemicolon
+	
+checkSpace:
+  li t1, 0x20
+  beq a0, t1, checkWordSpace
+  li s11, 0
+  jr x1
+  
+checkWordSpace:
+  bnez s11, test
+  j checkWord 
+  
+test:
+  li s11, 1
+  li s4, 0
+  j loop
+  
+checkWord:
+  ble s4, s3, newShortest
+  continueRecord:
+  li s4, 0
+  j loop
+	
+newShortest:
+  beqz s4, loop
+  mv s3, s4
+  mv s6, s5
+  sub s6, s6, s3
+  addi s6, s6, -1
+  li s4, 0
+  j loop
   
 end:
   li   a7, 57
-  mv   a0, s6
+  lw   a0, skaitymas	
   ecall
+  li   a7, 1024
+  la   a0, fin
+  li   a1, 0
+  ecall
+  la   t1, paskutinisSkaitymas    # deskriptoriaus adresas
+  sw   a0, (t1)                  # saugome deskriptorių
+  
+  li a7, 63
+  lw a0, paskutinisSkaitymas
+  la a1, buf
+  mv a2, s6
+  ecall 
+  
+  li a7, 63
+  lw a0, paskutinisSkaitymas
+  la a1, buf
+  mv a2, s3
+  ecall
+  
+  li a7, 64
+  lw a0, rasymas
+  la a1, result
+  li a2, 56
+  ecall
+  
+  li a7, 64
+  lw a0, rasymas
+  la a1, buf
+  mv a2, s3
+  ecall
+  
   li   a7, 93
   li   a0, 0
   ecall
-  
