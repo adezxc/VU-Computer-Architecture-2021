c923c4da36312c42b0e34688a750bceef22540f5
adezxc <adam.jasinski@unity3d.com>
2021-12-27 13:54:30 +0200

    l3 done and start of l4

--- L4/main.c
+++ L4/main.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <ctype.h>
+
+extern double opt(double* masyvas, uint64_t  kiek);
+extern uint64_t arvisi(double* masyvas, uint64_t kiek);
 
 int main() {
     FILE *fptr;
     int num, which = 0;
     fptr = fopen("input.dat", "r");
-    int n = fgetc(fptr);
-    printf("%c\n", n);
-    int size;
-    size = atoi(n);
-    double input[size];
+    int n;
+    fscanf(fptr, "%d", &n);
+    printf("%d\n", n);
+    double input[n];
 
-
-    while ( (num = fgetc(fptr)) != EOF ) {
-        if (!isspace(num)) {
-            int numInt = atoi(num);
-            input[which] = numInt;
-            which++;
-        }
-        else continue;
+    for (int i = 0; i < n; i++) {
+        double num;
+        fscanf(fptr, "%2lf", num);
+        input[i] = num;
     }
+
 
     fclose(fptr);
 
